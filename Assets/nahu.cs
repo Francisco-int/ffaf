310166b15c366f6231acc3230b23de345642acd2
videojuegoslagos <160129157+videojuegoslagos@users.noreply.github.com>
2024-04-03 20:13:33 -0300

    0.1

--- Assets/nahu.cs
+++ Assets/nahu.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class nahu : MonoBehaviour
 {
+
+    float movimientoHorizontal;
+    float movimientoVertical;
+    public float speed;
+    [SerializeField] Rigidbody rb;
+    float movimientoZ;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector3(0f, 0f, speed), ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector3(0f, 0f, -speed), ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(new Vector3(speed, 0f, 0f), ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector3(-speed, 0f, 0f), ForceMode.Acceleration);
+        }
+
+
+
     }
 }
